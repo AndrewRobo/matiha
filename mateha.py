178f9a54384660883ead22e99f21f4a4b9d1838f
andi god <andrewprokopchyk34@gmail.com>
2020-10-18 23:07:50 +0300

    сделан цикл for для четырёх примеров

--- mateha.py
+++ mateha.py
@@ -2,12 +2,20 @@
 
 import random
 print('немухлевать        ясно!!!')
-a=random.randint(100000,199999)
-# d=int(
-sp=[49,25,32,52,55,75,100]
-i=random.choice(sp)
-o=int(i)
-print(o)
-h=a*o
-print(h)
+sp=[3,4,6,7,8,9]
+for d in range(0,4):
+    # получаем  случайное целое
+    # число в диапазоне от ста
+    # тысяч до девятсот девяносто
+    # девяти тысяч девятсот девяносто девяти
+    a=random.randint(100000,999999)
+
+    i=random.choice(sp)
+    #print("i первая цифра",i)
+    f=random.choice(sp)
+    #print("f вторая цифра",f)
+    o=f*10+i
+    print(o)
+    h=a*o
+    print(h)
 
