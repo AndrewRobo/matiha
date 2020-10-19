5cc3df8e0d67e8bcf14ff94d704bd791c9d1e3cc
andi god <andrewprokopchyk34@gmail.com>
2020-10-19 16:54:43 +0300

    уменшили разрядность и пронумеровали списка заданий

--- mateha.py
+++ mateha.py
@@ -1,21 +1,19 @@
 #!/usr/bin/env python3
 
 import random
-print('немухлевать        ясно!!!')
+print('примеры')
 sp=[3,4,6,7,8,9]
-for d in range(0,4):
+for d in range(1,5):
     # получаем  случайное целое
     # число в диапазоне от ста
     # тысяч до девятсот девяносто
     # девяти тысяч девятсот девяносто девяти
-    a=random.randint(100000,999999)
-
+    a=random.randint(10000,99999)
     i=random.choice(sp)
     #print("i первая цифра",i)
     f=random.choice(sp)
     #print("f вторая цифра",f)
     o=f*10+i
-    print(o)
     h=a*o
-    print(h)
+    print(d,"-",h,":",o)
 
