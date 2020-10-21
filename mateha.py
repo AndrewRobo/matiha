f03ef5c94db15ef91f36358cb3e38e3d25e4bab0
andi god <andrewprokopchyk34@gmail.com>
2020-10-21 21:14:50 +0300

    убрали дублирование цифр в делителе

--- mateha.py
+++ mateha.py
@@ -2,17 +2,21 @@
 
 import random
 print('примеры')
-sp=[3,4,6,7,8,9]
+sp=[3,4,5,6,7,8,9]
 for d in range(1,5):
     # получаем  случайное целое
     # число в диапазоне от ста
     # тысяч до девятсот девяносто
     # девяти тысяч девятсот девяносто девяти
     a=random.randint(10000,99999)
+    
     i=random.choice(sp)
     #print("i первая цифра",i)
     f=random.choice(sp)
     #print("f вторая цифра",f)
+    while i==f:
+        print("i=",i,",f=",f,"если цифры равны то меняем одну из них")
+        f=random.choice(sp)
     o=f*10+i
     h=a*o
     print(d,"-",h,":",o)
